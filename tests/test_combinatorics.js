5af09e1cb471d05e273fffaa302a574d258fa970
Apurva Kumar <apurvakumar313@gmail.com>
2025-02-16 00:55:42 +0530

    Add tests for combinatorics module including GCD, modular exponentiation, Fibonacci, and sieve functions

---
+++ tests/test_combinatorics.js
@@ -0,0 +1,6 @@
+const mathToolkit = require('competitive-math-toolkit');
+
+console.log(mathToolkit.gcd(36, 60)); // Output: 12
+console.log(mathToolkit.modExp(2, 10, 1000000007)); // Output: 1024
+console.log(mathToolkit.nthFibonacci(10)); // Output: 55
+console.log(mathToolkit.sieve(50)); // Output: [2, 3, 5, 7, 11, ...]
