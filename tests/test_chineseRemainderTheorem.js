7fd492bd40cf7352573b55c99e46b6397f2d7d01
Apurva Kumar <apurvakumar313@gmail.com>
2025-02-16 11:39:19 +0530

    Add test for Chinese Remainder Theorem implementation

---
+++ tests/test_chineseRemainderTheorem.js
@@ -0,0 +1,6 @@
+const { chineseRemainderTheorem } = require('competitive-math-toolkit');
+
+let num = [3, 5, 7];
+let rem = [2, 3, 2];
+
+console.log(chineseRemainderTheorem(num, rem)); // Output: 23
