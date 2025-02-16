69c12faf35e347f6eb249b0638f6a67dbca1365f
Apurva Kumar <apurvakumar313@gmail.com>
2025-02-16 11:38:55 +0530

    Add Chinese Remainder Theorem module to index.js exports

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const combinatorics = require('./src/combinatorics');
 const matrixMath = require('./src/matrix_math');
 const fibonacci = require('./src/fibonacci');
 const graphMath = require('./src/graph_math');
+const  chineseRemainderTheorem  = require('./src/chineseRemainderTheorem');
 
 module.exports = {
     ...numberTheory,
     ...combinatorics,
     ...matrixMath,
     ...fibonacci,
-    ...graphMath
+    ...graphMath,
+    ...chineseRemainderTheorem
 };
