749f6efc82d39f7a68719bf5923de6bb567842f4
Apurva Kumar <apurvakumar313@gmail.com>
2025-02-17 20:10:10 +0530

    Add tests for isDivisible, findDivisors, and primeFactorization functions

--- tests/test_combinatorics.js
+++ tests/test_combinatorics.js
@@ -4,3 +4,6 @@ console.log(mathToolkit.gcd(36, 60)); // Output: 12
 console.log(mathToolkit.modExp(2, 10, 1000000007)); // Output: 1024
 console.log(mathToolkit.nthFibonacci(10)); // Output: 55
 console.log(mathToolkit.sieve(50)); // Output: [2, 3, 5, 7, 11, ...]
+console.log(mathToolkit.isDivisible(10, 5)); // Output: "Yes"
+console.log(mathToolkit.findDivisors(10)); // Output: [1, 2, 5, 10]
+console.log(mathToolkit.primeFactorization(60)); // Output: [2, 2, 3, 5]
